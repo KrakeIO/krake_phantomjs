de75fd54cd8f42d13a20cf6723ceeac45fbb7990
Gary Teh <garyjob@Garys-MacBook-Air.local>
2014-06-18 09:16:51 +0800

    server header update

--- middlewares/set_headers.js
+++ middlewares/set_headers.js
@@ -9,7 +9,7 @@ var setHeaders = function(page, krakeQueryObject, next) {
   domain_info = parseUri(krakeQueryObject.origin_url);
   if(!domain_info.host.match(/facebook.com/)) {
     page.settings['userAgent'] = 
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36';
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36';
   }
   next();
 }
