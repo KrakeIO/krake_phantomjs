b186f19cdcdaf284a7f08691b11de7753be06cdd
Gary Teh <garyjob@Garys-MacBook-Air.local>
2015-02-24 17:13:27 +0800

    better status update

--- middlewares/get_cookies.js
+++ middlewares/get_cookies.js
@@ -1,8 +1,16 @@
 // Clicks on the elements on the page
 var getCookies = function(page, krakeQueryObject, next) {
-  console.log('  Extracting COOKIES set by page');
+  console.log('  COOKIES Extraction');
   krakeQueryObject.jobResults || {};
-  krakeQueryObject.jobResults.cookies = page.cookies;
+ 
+  if(!krakeQueryObject.disable_cookies) {
+    console.log('    getting those set by page');
+    krakeQueryObject.jobResults.cookies = page.cookies;
+  } else {
+    console.log('    disabled');
+    krakeQueryObject.jobResults.cookies = [];
+  }
+
   next();
 }
 
--- middlewares/set_cookies.js
+++ middlewares/set_cookies.js
@@ -2,9 +2,16 @@
 // @param : page:object
 // @param : cookies:array[]
 var setCookies = function(page, krakeQueryObject, next) {
+  console.log('  COOKIES Setting');  
+  if(krakeQueryObject.disable_cookies) {
+    console.log('    disabled');
+    phantom.cookiesEnabled = false;
+  }
   
-  if(krakeQueryObject.cookies) {
+  if(!krakeQueryObject.disable_cookies && krakeQueryObject.cookies) {
+    console.log('    setting cookies provided');
     for( x = 0; x < krakeQueryObject.cookies.length; x++) {
+      console.log('      ' + krakeQueryObject.cookies[x].name + ' : ' + krakeQueryObject.cookies[x].value);
       add_results = phantom.addCookie({
         name : krakeQueryObject.cookies[x].name, 
         value : krakeQueryObject.cookies[x].value, 
