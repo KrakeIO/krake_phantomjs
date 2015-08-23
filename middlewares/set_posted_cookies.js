5862126d935c7cd4ff1c2d3e652de5828071cc3b
Gary Teh <garyjob@x.199.87.85.web-pass.com>
2015-08-23 15:40:07 -0700

    updated to support post cookie

--- middlewares/set_posted_cookies.js
+++ middlewares/set_posted_cookies.js
@@ -1,9 +1,14 @@
+var KSON = require('../node_modules/kson/lib/kson');
+
 var setPostedCookies = function(page, krakeQueryObject, next) {
   console.log('  Posted COOKIES Setting');  
-
   if(krakeQueryObject.set_post_cookies) {
+    console.log('    cookie set to POST Body post_cookie');
     krakeQueryObject.post_data = krakeQueryObject.post_data || {}
-    krakeQueryObject.post_data["post_cookies"] = JSON.stringify( krakeQueryObject.cookies )
+    krakeQueryObject.post_data["post_cookies"] = encodeURIComponent ( JSON.stringify( krakeQueryObject.cookies ) )
+  } else {
+    console.log(JSON.stringify(krakeQueryObject))    
+    console.log('    not set');
   }
 
   next();    
